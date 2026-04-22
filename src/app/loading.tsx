709377f528ed29c540e7c48c297489665b63c9d3
Benaiah15 <benaiahajibade@gmail.com>
2026-04-22 22:40:07 +0100

    Updated loader to use brand red color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-12 h-12 border-4 border-gray-600 border-t-blue-500 rounded-full animate-spin"></div>
+    <div className="flex min-h-[60vh] items-center justify-center">
+      {/* Changed border-t-blue-600 to border-t-red-600 */}
+      <div className="h-12 w-12 animate-spin rounded-full border-4 dark:border-zinc-800 border-gray-200 border-t-red-600"></div>
     </div>
   );
 }
