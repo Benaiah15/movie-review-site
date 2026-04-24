a1c174dc1c4bbb2087de82f8837e18c099dd5f5a
Benaiah15 <benaiahajibade@gmail.com>
2026-04-24 22:49:36 +0100

    Exported useUploadThing hook in lib to support custom Avatar UI

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,10 +1,12 @@
-"use client";
-
 import {
   generateUploadButton,
   generateUploadDropzone,
 } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+
+// THIS IS THE MISSING PIECE WE JUST ADDED:
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
